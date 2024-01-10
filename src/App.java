8f04a142bf2b95c08662883d88f95cf08eb3223a
gabriella jonsson <gabriella.jonsson@elev.ga.ntig.se>
2024-01-10 10:35:25 +0100

    Välja ett spelnamn till spelare 1

--- src/App.java
+++ src/App.java
@@ -1,5 +1,13 @@
+import java.util.Scanner;
 public class App {
     public static void main(String[] args) throws Exception {
         System.out.println("Hello, World!");
-    }
+        Scanner reader = new Scanner(System.in);
+
+        System.out.println("Fight spel!");
+        System.out.println("Välj ett spelnamn för spelare 1:");
+
+        
+    }   
+    
 }
