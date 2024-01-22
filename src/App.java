d88aff98bfb5316637f25208f1c35f64998134f8
gabriella jonsson <gabriella.jonsson@elev.ga.ntig.se>
2024-01-22 08:32:37 +0100

    Skapat meny

--- src/App.java
+++ src/App.java
@@ -1,3 +1,5 @@
+// Gabriella Jonsson
+// Te22a
 import java.util.Scanner;
 public class App {
     public static void main(String[] args) throws Exception {
@@ -6,33 +8,12 @@ public class App {
 
         System.out.println("Gabriellas Fight spel!");
         System.out.print("Välj ett spelnamn för spelare 2:");
-
         String usernamne = reader.nextLine();
+
         System.out.println("Använarnamn för spelare 2:" + usernamne);
-        System.out.println("Användarman för spelare 1: Sam");
-
-        System.out.println(" vilken Spelare ska slå första slaget? svara 1 eller 2");
-        String svar = reader.nextLine();
-
-        if( svar = 2) 
-        {
-            System.out.println("Spelare 2 slog spelare 1 i ansiktet!");
-        }
-        else{
-            System.out.println("Spelare 1 sparkade Spelare 2 i revbenen men missade...");
-        }
+        System.out.println("Användarman för spelare 1: Sam"); 
         
-        System.out.println("Spelare 2 är påväg att sparka dig i höften! Ska du 1: ta sparken eller 2: blocka sparken?");
-        String svar = reader.nextLine()
-
-        if(svar=1)
-        {
-           System.out.println("Du bröt benet och du blöder ut på makren för att sam sparkade dig, Game over!");
-        }
-        else{
-            System.out.println("Du fick ont med klarade dig, den hät gången...");
-        }
-        
-    }   
+    
+       
     
 }
