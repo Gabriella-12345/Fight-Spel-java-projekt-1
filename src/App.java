22f065752f707f7d39ca4ed8403989c2f94de4f6
gabriella jonsson <gabriella.jonsson@elev.ga.ntig.se>
2024-01-30 20:54:20 +0100

    fixa hp(hälsopoäng)

--- src/App.java
+++ src/App.java
@@ -7,11 +7,8 @@ public class App {
         System.out.println("Hello, World!");
         Scanner reader = new Scanner(System.in);
         Random rand = new Random(); 
-        int hp1; // hälsopoäng för sam/ spelare 1
-        int hp2; // hälsopoäng för spelare 2
-
-        hp1 = 10;
-        hp2 = 10;
+        int hp1 = 10; // hälsopoäng för sam/ spelare 1
+        int hp2 = 10; // hälsopoäng för spelare 2
 
         System.out.println("SLÅS MED SAM!" + "\n");
 
@@ -23,29 +20,34 @@ public class App {
         
         System.out.println("Vilken Spelare ska slå första slaget? svara 1 eller 2 ");
         String svar = reader.nextLine();
-        if( svar == "2") 
+        if( svar == "2" ) 
             {
                 System.out.println("Du slog Sam i ansiktet!");
-                
+                hp1--;
             }
             else{
                 System.out.println("Sam sparkade dig i revbenen men missade..." + "\n\n");
+                hp2--;
             }
-
+            System.out.println(hp1);
+            System.out.println(hp2);
         int ettrandomInt = rand.nextInt(11); // genererar ett slumptal mellan 0-10
         int tvårandomInt = rand.nextInt(11); // genererar ett slumptal mellan 0-10
        
-        System.out.println("Sam är påväg att sparka dig i höften! Kommer du ta eller blocka sparken?");
-        
+        System.out.println("Sam är påväg att sparka dig i höften! Kommer du ta eller blocka sparken? Tryck Enter för att fortsätta");
+        String enter = reader.nextLine();
+
         if(ettrandomInt>=tvårandomInt) // Jämför de två slumpmässigt genererade talen, spelaren med mindre talet förlorar ett hälsopoäng
             {
-                System.out.println("Du bröt benet och du blöder ut på makren för att sam sparkade dig!");
-               // förlora hälso poäng -1
+                System.out.println("Du bröt benet och du blöder ut på makren för att sam sparkade dig! Oh nej");
+                hp2--; // förlora hälso poäng -1
             }
             else{
                 System.out.println("Du fick ont med klarade dig, den här gången..." + "\n\n");
                // ingen förlorar hälso poäng
             }
+            System.out.println(hp1);
+            System.out.println(hp2);
         
         
     }   
